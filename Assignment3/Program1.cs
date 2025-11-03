d92850ca5f0d79d8522892d09f88ce90af84901d
Navdeepverma74 <navdeep.verma_mca24@gla.ac.in>
2025-11-03 17:17:57 +0530

    Add files via upload

---
+++ Assignment3/Program1.cs
@@ -0,0 +1,45 @@
+﻿// 1. Create a class called Employee with properties for name, age, and salary. Implement a method to
+//display employee details.
+using System;
+using System.Xml.Linq;
+
+namespace Assignment3
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.name = "Navdeep Verma";
+            emp.age = 23;
+            emp.salary = 80000.0f;
+            Employee emp1 = new Employee();
+            emp1.name = "Navdeep";
+            emp1.age = 21;
+            emp1.salary = 70000.0f;
+            emp.Display();
+            emp1.Display();
+
+        }
+    }
+
+    public class Employee
+    {
+        
+        public string name;
+        public int age;
+        public double salary;
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Details:");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Salary: " + salary);
+            Console.WriteLine();
+        }
+
+    }
+
+
+}
